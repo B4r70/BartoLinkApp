e24da0c3b92d495ae45797f270781c08f38702c6
Bartosz Stryjewski <bartosz@stryjewski.email>
2026-04-30 17:58:19 +0200

    Initial Commit

---
+++ bartolink/bartolinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  bartolinkApp.swift
+//  bartolink
+//
+//  Created by Barto on 30.04.26.
+//
+
+import SwiftUI
+
+@main
+struct bartolinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
